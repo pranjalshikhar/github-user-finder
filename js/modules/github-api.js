6cd396016c55d7d7546c24a878b745874a3e81d7
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2021-05-05 11:28:04 +0530

    Update github-api.js

--- js/modules/github-api.js
+++ js/modules/github-api.js
@@ -20,22 +20,22 @@ export default class GitHubApi {
                     if (json.message !== 'Not Found') {
                         const { login, name, bio, followers, following, html_url, avatar_url } = json;
                         this.templateUser = `
-              <section class="user-data">
-                <div class="column">
-                  <a href="${html_url}" class="username">@${login}</a>
-                  <h1 class="main-title">${name}</h1>
-                  <h2 class="title user-desc">${bio ? bio : ''}</h2>
-                  <strong class="follow">${followers} followers | ${following} following</strong>
-                  <div class="options">
-                    <a href="#repositories" class="btn btn-rep">Repositories</a>
-                    <a href="" class="btn btn-search">New Search</a>
-                  </div>
+            <section class="user-data">
+              <div class="column">
+                <a href="${html_url}" class="username">@${login}</a>
+                <h1 class="main-title">${name}</h1>
+                <h2 class="title user-desc">${bio ? bio : ''}</h2>
+                <strong class="follow">${followers} followers | ${following} following</strong>
+                <div class="options">
+                  <a href="#repositories" class="btn btn-rep">Repositories</a>
+                  <a href="" class="btn btn-search">New Search</a>
                 </div>
-                <figure class="user-img">
-                  <img src="${avatar_url}" alt="${name}">
-                </figure>
-              </section>
-            `;
+              </div>
+              <figure class="user-img">
+                <img src="${avatar_url}" alt="${name}">
+              </figure>
+            </section>
+          `;
 
                         this.structure = document.querySelector('.structure');
                         this.structure.classList.remove('main');
@@ -58,26 +58,26 @@ export default class GitHubApi {
             .then(json => {
                 json.forEach(({ name, stargazers_count, forks, description, language, html_url }) => {
                     this.templateRepositories += `
-            <a href="${html_url}" target="_blank" class="rep-item">
-              <h2 class="rep-title">${name}</h2>
-              <p class="paragraph p-rep">${description ? description : ''}</p>
-              <ul class="rep-info">
-                <li>${language ? language : ''}</li>
-                <li class="stars">${stargazers_count}</li>
-                <li class="forks">${forks}</li>
-              </ul>
-            </a>
-          `;
+          <a href="${html_url}" target="_blank" class="rep-item">
+            <h2 class="rep-title">${name}</h2>
+            <p class="paragraph p-rep">${description ? description : ''}</p>
+            <ul class="rep-info">
+              <li>${language ? language : ''}</li>
+              <li class="stars">${stargazers_count}</li>
+              <li class="forks">${forks}</li>
+            </ul>
+          </a>
+        `;
                 });
 
                 this.structure.innerHTML += `
-          <section class="repositories" id="repositories">
-            <h1 class="main-title">All your repositories</h1>
-            <div class="grid-rep">
-              ${this.templateRepositories.replace('undefined', '')}
-            </div>
-          </section>
-        `;
+        <section class="repositories" id="repositories">
+          <h1 class="main-title">All your repositories</h1>
+          <div class="grid-rep">
+            ${this.templateRepositories.replace('undefined', '')}
+          </div>
+        </section>
+      `;
 
                 const backButton = this.structure.querySelector('.btn-search');
                 backButton.addEventListener('click', window.onload);
